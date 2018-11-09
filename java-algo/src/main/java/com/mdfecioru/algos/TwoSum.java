8200ada7442e47a2bfd86ea4b17d4238821dc3aa
mdanielf <mdanielf@mdanielfs-mbp.corp.adobe.com>
2018-11-08 18:55:56 -0800

    2Sum implementation

---
+++ java-algo/src/main/java/com/mdfecioru/algos/TwoSum.java
@@ -0,0 +1,25 @@
+package com.mdfecioru.algos;
+
+import java.util.HashSet;
+
+public class TwoSum {
+
+    private HashSet<Long> hashSet;
+
+    public TwoSum() {
+        hashSet = new HashSet<>();
+    }
+
+    public void addElem (Long elem) {
+        hashSet.add(elem);
+    }
+
+    public boolean hasTwoSum(Long targetSum) {
+        for (Long elem1: hashSet) {
+            Long elem2 = targetSum - elem1;
+            if ((elem1.compareTo(elem2) != 0) && (hashSet.contains(elem2))) return true;
+        }
+        return false;
+    }
+
+}
---
+++ java-algo/src/test/java/com/mdfecioru/algos/TwoSumTest.java
@@ -0,0 +1,57 @@
+package com.mdfecioru.algos;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import static org.junit.Assert.*;
+
+public class TwoSumTest {
+
+    @Test
+    public void twoSumTest1() {
+        TwoSum twoSum = new TwoSum();
+
+        twoSum.addElem(2L);
+        twoSum.addElem(3L);
+        twoSum.addElem(4L);
+        twoSum.addElem(4L);
+        twoSum.addElem(9L);
+        twoSum.addElem(4L);
+        twoSum.addElem(10L);
+
+        assertTrue(twoSum.hasTwoSum(7L));
+        assertFalse(twoSum.hasTwoSum(8L));
+    }
+
+    @Test
+    public void twoSumTestFile1 () {
+
+        TwoSum twoSum = new TwoSum();
+
+        try {
+            String line;
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    this.getClass().getResourceAsStream("../../../2sum.txt")));
+            line = in.readLine();
+            while (line != null) {
+                Long nextNumber = Long.parseLong(line);
+                twoSum.addElem(nextNumber);
+                line = in.readLine();
+            }
+
+            int matches = 0;
+            for (int i=-10000; i<=10000; i++) {
+                if (twoSum.hasTwoSum(new Long(i))) matches++;
+            }
+
+            assertEquals(23, matches);
+
+        }
+        catch (Exception ex) {
+            System.out.println("ERROR: " + ex);
+        }
+    }
+
+}
